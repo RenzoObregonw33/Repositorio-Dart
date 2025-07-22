c9dcc2b63f523bba57f449fa20b419654860f163
AcunaC <billykentac@gmail.com>
2025-07-22 15:04:33 -0500

    Login y home Listos

--- login/lib/screens/home_screen.dart
+++ login/lib/screens/home_screen.dart
@@ -42,16 +42,43 @@ class HomeScreem extends StatelessWidget {
           children: [
             Row(                                   //organiza en una fila horizontal   
               children: [
-                CircleAvatar(                      //Icono circular del Usuario 
-                  radius: 30,
-                  backgroundColor:  Color(0xFFF3C86C),
-                  child: Text('V', style: TextStyle(color: Colors.white, fontSize: 30,fontWeight: FontWeight.bold)),
+                Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: Colors.green, // Color del borde
+                      width: 3.0,          // Grosor del borde
+                    ),
+                  ),
+                  child:  CircleAvatar(                      //Icono circular del Usuario 
+                    radius: 30,
+                    backgroundColor:  Colors.transparent,
+                    //child: Text('V', style: TextStyle(color: Colors.black, fontSize: 30,fontWeight: FontWeight.bold)),
+                    child: ClipOval(
+                      child: Image.asset(
+                        'assets/MG.jpeg', // Asegúrate de que la imagen esté en la carpeta assets
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.cover, // Ajusta la imagen para que ocupe todo el círculo
+                      ),
+                    ),
+                  ),
                 ),
                 SizedBox(width: 16),
                 Expanded(
-                  child: Text(
-                    '$nombre $apellido',
-                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500, fontFamily: 'Inter'),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [Text(
+                        '$nombre $apellido',
+                        style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500, fontFamily: 'Inter'),
+                      ),
+                      Text(
+                        'ADMINISTRADOR',
+                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.w400, fontFamily: 'Inter'),
+                      ),
+                    ]
                   ),
                 ),
               ],
@@ -59,12 +86,13 @@ class HomeScreem extends StatelessWidget {
             SizedBox(height: 24),                  //texto 
             Text(
               'Mis Organizaciones:',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, fontFamily: 'Inter'),
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Inter'),
             ),
-            SizedBox(height: 10),
+            SizedBox(height: 20),
             Expanded(                               //esto crea una lista 
-              child: ListView.builder(              //crea Listas Dinamicas jala datos de una BD o una API
+              child: ListView.separated(              //crea Listas Dinamicas jala datos de una BD o una API
                 itemCount: organizaciones.length,
+                separatorBuilder: (context, index) => const SizedBox(height: 15),
                 itemBuilder: (context, index) {
 
                   final org = organizaciones[index];
@@ -88,14 +116,24 @@ class HomeScreem extends StatelessWidget {
                       );
                     },
                     child: Card(
-                      color: Colors.orange,
-                      shadowColor: Color(0xFFF3B83C),
-                      elevation: 4,
+                      color: Color(0xFFF8A835),
+                      shadowColor: Colors.black, // Color de sombra
+                      elevation: 3,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8), // Tarjeta cuadrada
-                        side: BorderSide(color: const Color(0xFFFFF8EB),width: 2),
+                        borderRadius: BorderRadius.circular(20), // Tarjeta cuadrada
+                        
                       ),
                       child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [ Color(0xFFFFA528), Color(0xFFF77B09),],
+                            stops: [0.1, 0.9],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          color: Color(0xFFF8A835),
+                          borderRadius: BorderRadius.circular(20), // Tarjeta cuadrada
+                        ),
                         width: double.infinity,
                         height: 120,
                         padding: const EdgeInsets.all(16),
@@ -105,14 +143,14 @@ class HomeScreem extends StatelessWidget {
                           children: [
                             Row(
                               children: [
-                                Icon(Icons.domain, color: Color(0xFFF3B83C), size: 28),
+                                Icon(Icons.domain, color: Colors.black, size: 28),
                                 SizedBox(width: 15),
                                 Expanded(
                                   child: Text(
                                     razonSocial,
                                     style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
+                                      color: Colors.white,
+                                      fontSize: 20,
                                       fontWeight: FontWeight.bold,
                                       fontFamily: 'Inter',
                                     ),
@@ -125,14 +163,16 @@ class HomeScreem extends StatelessWidget {
                               'RUC: $ruc',
                               style: TextStyle(
                                 fontFamily: 'Inter',
-                                color: Colors.grey.shade800,
+                                color: Colors.white,
+                                fontSize: 14,
                               ),
                             ),
                             Text(
                               'ID: $id',
                               style: TextStyle(
                                 fontFamily: 'Inter',
-                                color: Colors.grey.shade800,
+                                color: Colors.white,
+                                fontSize: 14,
                               ),
                             ),
                           ],
--- login/lib/widgets/grafico_embudo.dart
+++ login/lib/widgets/grafico_embudo.dart
@@ -16,18 +16,26 @@ class GraficoEmbudo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SfFunnelChart(    
-      title: ChartTitle(text: 'Cumplimiento Laboral', textStyle: TextStyle(fontWeight: FontWeight.bold)),
+    return SfFunnelChart(
+      title: ChartTitle(
+        text: 'Cumplimiento Laboral',
+        textStyle: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
       tooltipBehavior: TooltipBehavior(enable: true),
       series: FunnelSeries<FunnelData, String>(
         dataSource: data,
         xValueMapper: (FunnelData d, _) => d.label,
         yValueMapper: (FunnelData d, _) => d.value,
         pointColorMapper: (FunnelData d, _) => d.color,
-        gapRatio: 0.03,
-        neckWidth: '15%',
-        neckHeight: '0%',
-        dataLabelSettings: const DataLabelSettings(
+        gapRatio: 0.2,  // Aumenta este valor para más separación entre segmentos
+        neckWidth: '20%', // Ajusta el ancho del cuello
+        neckHeight: '15%', // Ajusta la altura del cuello
+        explode: true,  // Separa ligeramente los segmentos
+        explodeOffset: '5%',  // Controla la cantidad de separación
+        dataLabelSettings: DataLabelSettings(
           isVisible: true,
           labelPosition: ChartDataLabelPosition.inside,
           textStyle: TextStyle(
